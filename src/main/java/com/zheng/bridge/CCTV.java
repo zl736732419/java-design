536d6beaa8d4e2ab75526125140e3229f9e5547b
zhenglian <736732419@qq.com>
2018-04-04 16:06:44 +0800

    修改桥接模式注释

--- src/main/java/com/zheng/bridge/CCTV.java
+++ src/main/java/com/zheng/bridge/CCTV.java
@@ -3,6 +3,7 @@ package com.zheng.bridge;
 import javax.swing.*;
 
 /**
+ * 桥接模式中存在多维度变化，只保留其中一维变化，将其他维度的变化都抽象出来形成单独的变化维度
  * Created by zhenglian on 2016/10/30.
  */
 public abstract class CCTV extends JPanel {
