a28a8edf422daaa3bd71bcbb8ea4035d89c4541b
zhenglian <736732419@qq.com>
2018-04-04 16:45:54 +0800

    修改享元模式

--- src/main/java/com/zheng/flyweight/CarAttrFactory.java
+++ src/main/java/com/zheng/flyweight/CarAttrFactory.java
@@ -1,41 +1,39 @@
 package com.zheng.flyweight;
 
-import com.google.common.collect.Maps;
+import com.google.common.base.Optional;
 
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 创建享元对象的工厂，保证享元对象唯一
  * Created by zhenglian on 2016/10/30.
  */
 public class CarAttrFactory {
-    private static CarAttrFactory factory = null;
-    private Map<String, CarAttr> attrs = Maps.newHashMap();
+    private ConcurrentHashMap attrs = new ConcurrentHashMap();
 
     private CarAttrFactory() {
     }
 
-    public static CarAttrFactory getInstance() {
-        if(null == factory) {
-            factory = new CarAttrFactory();
-        }
+    private static class CarAttrFactoryHolder {
+        public static CarAttrFactory factory = new CarAttrFactory();
+    }
 
-        return factory;
+    public static CarAttrFactory getInstance() {
+        return CarAttrFactoryHolder.factory;
     }
 
     public CarAttr getCarAttr(String key) {
-        if(attrs.keySet().contains(key)) {
-            return attrs.get(key);
+        CarAttrImpl carAttr = (CarAttrImpl) attrs.get(key);
+        if (Optional.fromNullable(carAttr).isPresent()) {
+            return carAttr;
         }
 
         String[] str = key.split("#");
         double width = Double.parseDouble(str[0]);
         double height = Double.parseDouble(str[1]);
         double length = Double.parseDouble(str[2]);
-
-        CarAttr carAttr = new CarAttrImpl(width, height, length);
-
-        attrs.put(key, carAttr);
+        carAttr = new CarAttrImpl(width, height, length);
+        attrs.putIfAbsent(key, carAttr);
         return carAttr;
 
     }
