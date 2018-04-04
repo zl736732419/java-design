66ef6f7e3f0e76ab79610c9bd56de466f93388e8
zhenglian <736732419@qq.com>
2018-04-04 15:58:14 +0800

    修改适配器模式注释

--- src/main/java/com/zheng/adapter/ThreeToTwoOutletAdapter.java
+++ src/main/java/com/zheng/adapter/ThreeToTwoOutletAdapter.java
@@ -1,6 +1,8 @@
 package com.zheng.adapter;
 
 /**
+ * 适配器模式常用于兼容不同接口的情况
+ * 因为某些原因无法对已有的接口进行变动，只能通过提供新的接口来适配已经存在但又不满足条件的接口
  * Created by zhenglian on 2016/10/21.
  */
 public class ThreeToTwoOutletAdapter implements ThreeElectricOutlet {
