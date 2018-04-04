b7428c8126d8d4841b3a8fd5baf9134bcafcb923
zhenglian <736732419@qq.com>
2018-04-04 15:52:16 +0800

    修改工厂方法和抽象工厂模式注释

--- src/main/java/com/zheng/abstractfactory/ClothesAbstractFactory.java
+++ src/main/java/com/zheng/abstractfactory/ClothesAbstractFactory.java
@@ -1,7 +1,10 @@
 package com.zheng.abstractfactory;
 
 /**
- * 抽象工厂，定义创建衣服和鞋子的接口
+ * 抽象工厂意在创建一个产品系列，
+ * 它针对的是面向多个产品等级结构的
+ * 当发现存在一组接口有多种实现，则可以考虑用抽象工厂模式
+ * 
  * Created by zhenglian on 2016/10/23.
  */
 public abstract class ClothesAbstractFactory {
--- src/main/java/com/zheng/factorymethod/AbstractCarFactory.java
+++ src/main/java/com/zheng/factorymethod/AbstractCarFactory.java
@@ -1,6 +1,8 @@
 package com.zheng.factorymethod;
 
 /**
+ * 工厂方法模式针对的是一个产品等级结构
+ * 当发现存在一个接口有多种实现的方式，则可以考虑用工厂方法来实现
  * Created by zhenglian on 2016/10/23.
  */
 public abstract class AbstractCarFactory {
